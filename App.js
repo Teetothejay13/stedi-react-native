74c2703b27f0b91e126f7c2caf4041fcbc36627f
TJ <teetothejay13@gmail.com>
2022-03-02 13:04:43 -0700

    began code updating

--- App.js
+++ App.js
@@ -13,46 +13,56 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName='Home'
-        activeColor='white'
-        barStyle={{ backgroundColor: 'green' }}
-      >
-        <Tab.Screen
-          name='Home'
-          component={Home}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='home' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Step Counter'
-          component={Counter}
-          options={{
-            tabBarLabel: 'Step Counter',
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name='watch' color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name='Settings'
-          component={SettingsScreen}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({ color }) => (
-              <FontAwesome name='gear' color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  //const [userLoggedIn, setUserLoggedIn] = useState(false)
+  //if(userLoggedIn){
+    //console.log("User Logged In: "+userLoggedIn)
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName='Home'
+          activeColor='white'
+          barStyle={{ backgroundColor: 'green' }}
+        >
+          <Tab.Screen
+            name='Home'
+            component={Home}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='home' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Step Counter'
+            component={Counter}
+            options={{
+              tabBarLabel: 'Step Counter',
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name='watch' color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name='Settings'
+            component={SettingsScreen}
+            options={{
+              tabBarLabel: 'Settings',
+              tabBarIcon: ({ color }) => (
+                <FontAwesome name='gear' color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  //}
+  //else {
+        //return(<View> <Text>This is the Login Screen</Text>
+        //<Button title="Log In" onPress={setUserLoggedIn(True)}>
+        //</Button></View>);
+  //}
+  
 
 
 }
